166b83da0baa750f5e065a3ae45b6accb7e141f7
Mohamed Ahmed <mohamed71992@gmail.com>
2016-01-31 18:24:36 +0200

    Issue #1, Adding all the drivers required by the watchdog

---
+++ ARM/Drivers/watchdog/watchdog_test.c
@@ -0,0 +1,8 @@
+/*
+ * watchdog_test.c
+ *
+ *  Created on: Jan 31, 2016
+ *      Author: mido4
+ */
+
+
---
+++ ARM/Drivers/watchdog/watchdog_test.h
@@ -0,0 +1,13 @@
+/*
+ * watchdog_test.h
+ *
+ *  Created on: Jan 31, 2016
+ *      Author: mido4
+ */
+
+#ifndef DRIVERS_WATCHDOG_WATCHDOG_TEST_H_
+#define DRIVERS_WATCHDOG_WATCHDOG_TEST_H_
+
+
+
+#endif /* DRIVERS_WATCHDOG_WATCHDOG_TEST_H_ */
