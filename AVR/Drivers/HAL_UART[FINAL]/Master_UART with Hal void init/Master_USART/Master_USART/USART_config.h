8a47da5f39ef9480ca4758773529ef97ed9fa157
MarkMagdyAntoun <mark_antoun_zakak@hotmail.com>
2016-06-18 00:22:47 +0200

    HAL_UART_finished

---
+++ AVR/Drivers/HAL_UART[FINAL]/Master_UART with Hal void init/Master_USART/Master_USART/USART_config.h
@@ -0,0 +1,24 @@
+/*
+ * USART_config.h
+ *
+ * Created: 2/4/2016 2:49:13 PM
+ *  Author: Mark
+ */ 
+
+
+#ifndef USART_CONFIG_H_
+#define USART_CONFIG_H_
+
+#include <stdint.h>
+
+void uart_disable();
+void uart_disable_rx();
+void uart_disable_tx();
+void uart_enable();
+void uart_enable_rx();
+void uart_enable_tx();
+void uart_disable_interrupt();
+void uart_enable_interrupt();
+void uart_enable_interrupt_rx();
+
+#endif /* USART_CONFIG_H_ */
