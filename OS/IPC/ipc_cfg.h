e07d57512099473a4f0164febe69dacfc5969f38
Mohamed <mohamed71992@gmail.com>
2016-06-22 21:17:42 +0200

    Merge ipc config with the rtos config

--- OS/IPC/ipc_cfg.h
+++ OS/IPC/ipc_cfg.h
@@ -20,84 +20,18 @@
  *  distribution.
  *****************************************************************************/
 
+#ifndef IPC_CFG_H_
+#define IPC_CFG_H_
+
 #include <stdint.h>
+#include "../RTOS.h"
+#include "ipc_trace.h"
 
-#define IPC_CFG_H
-
-/* the basetype must be defined some where */
-typedef int8_t BaseType_t ;		// signed basetype is the base datatype
+typedef uint8_t TimeOut_t;
 typedef uint8_t TickType_t ;	// ticktype is a structure for the tick
-typedef uint8_t UBaseType_t ; 	// unsigned basetype is important datatype used
-typedef uint8_t TimeOut_t;  	// do not know want to know more about it
 
-#define configASSERT(x)			// configAssert check the condition x is true or return the file and line number
-#define taskENTER_CRITICAL()	// enable critical section
-#define taskEXIT_CRITICAL()		// disable critical section
-#define pdTRUE	0x01			// true return
-#define pdFALSE	0x00			// false return
-#define pdPASS	0x01			// return value that the function carried out correctly
-#define pdFAIL 	0x00			// return fail
-#define mtCOVERAGE_TEST_MARKER()// just an empty for the else part of the if statement
-#define PRIVILEGED_FUNCTION		// to the memory protection unit
-#define configUSE_PREEMPTION 0	// config the scheduler to be preemptive or cooperative
-#define portYIELD_WITHIN_API()	// check for the tasks to take the higher priority one
 #define errQUEUE_FULL	0x00	// error value indicates that the queue is full
 #define errQUEUE_EMPTY 	0x00	// error the queue is empty
 #define configUSE_QUEUE_LOCKS 1	// configure the locks
 
-#ifndef traceQUEUE_CREATE
-	#define traceQUEUE_CREATE( pxNewQueue )
 #endif
-
-#ifndef traceQUEUE_CREATE_FAILED
-	#define traceQUEUE_CREATE_FAILED( ucQueueType )
-#endif
-
-#ifndef traceQUEUE_SEND
-	#define traceQUEUE_SEND( pxQueue )
-#endif
-
-#ifndef traceQUEUE_SEND_FAILED
-	#define traceQUEUE_SEND_FAILED( pxQueue )
-#endif
-
-#ifndef traceQUEUE_RECEIVE
-	#define traceQUEUE_RECEIVE( pxQueue )
-#endif
-
-#ifndef traceQUEUE_PEEK
-	#define traceQUEUE_PEEK( pxQueue )
-#endif
-
-#ifndef traceQUEUE_PEEK_FROM_ISR
-	#define traceQUEUE_PEEK_FROM_ISR( pxQueue )
-#endif
-
-#ifndef traceQUEUE_RECEIVE_FAILED
-	#define traceQUEUE_RECEIVE_FAILED( pxQueue )
-#endif
-
-#ifndef traceQUEUE_SEND_FROM_ISR
-	#define traceQUEUE_SEND_FROM_ISR( pxQueue )
-#endif
-
-#ifndef traceQUEUE_SEND_FROM_ISR_FAILED
-	#define traceQUEUE_SEND_FROM_ISR_FAILED( pxQueue )
-#endif
-
-#ifndef traceQUEUE_RECEIVE_FROM_ISR
-	#define traceQUEUE_RECEIVE_FROM_ISR( pxQueue )
-#endif
-
-#ifndef traceQUEUE_RECEIVE_FROM_ISR_FAILED
-	#define traceQUEUE_RECEIVE_FROM_ISR_FAILED( pxQueue )
-#endif
-
-#ifndef traceQUEUE_PEEK_FROM_ISR_FAILED
-	#define traceQUEUE_PEEK_FROM_ISR_FAILED( pxQueue )
-#endif
-
-#ifndef traceQUEUE_DELETE
-	#define traceQUEUE_DELETE( pxQueue )
-#endif
-
