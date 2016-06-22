66bc59ae091a34c2993d8401ac2bb13ba6d6f1ad
Mohamed <mohamed71992@gmail.com>
2016-06-22 04:15:16 +0200

    Merging HAL code for both tiva and Avr

--- HAL/TivaHAL/Debug/tm4c123gh6pm_startup_ccs.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c
-tm4c123gh6pm_startup_ccs.obj: E:/Code\ Composer/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
-
-../tm4c123gh6pm_startup_ccs.c: 
-E:/Code\ Composer/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
